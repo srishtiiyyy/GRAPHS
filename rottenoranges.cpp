df8dba814a2852e017bfdbee364e3b4882382ef2
Srishti <srishtisingh910@gmail.com>
2025-10-12 21:08:31 +0530

    ROTTEN ORANGES

--- rottenoranges.cpp
+++ rottenoranges.cpp
@@ -1,7 +1,6 @@
 /*
-------------------------------------------------------------
+
 🧩 Problem: Rotten Oranges (LeetCode 994)
-------------------------------------------------------------
 You are given an m x n grid where each cell can have one of three values:
 
 0 → Empty cell  
@@ -13,7 +12,6 @@ Every minute, any fresh orange that is 4-directionally adjacent to a rotten one 
 Return the **minimum number of minutes** that must elapse until no cell has a fresh orange.  
 If it is **impossible** to rot all oranges, return **-1**.
 
-------------------------------------------------------------
 Example:
 Input:
 grid = [
